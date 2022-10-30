27adc66441e178d87578b092983f00e59f6c0b47
Aljoberg <72980003+Aljoberg@users.noreply.github.com>
2022-10-30 18:49:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const app= express();
 app.all("/test", (req, res) => res.send({"debugToken": process.env.token}))
 
 app.get("/",(req,res)=>{
-  res.send("hello"const express = require("express");
-const app= express();
-
-app.all("/test", (req, res) => res.send({"debugToken": process.env.token}))
-
-app.get("/",(req,res)=>{
   res.send("hello")
 })
 //debug things lol
@@ -17,13 +11,13 @@ app.listen(3000, () => {
   console.log("project is running!");
 })
 const Discord = require("discord.js");
- const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]}); 
+ const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 client.on("ready", () => console.log("ready"))
 
 client.on("messageCreate", message => {
   if(message.channel.id == "1034940722866049205") {
     if(message.content.toLowerCase() != "wooly") message.delete();
   }
-}) 
+});
 
 client.login(process.env.token);
